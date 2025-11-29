0288af82f0f608d3b9f4c44dae05cd224f169ced
wy-mall <2952282525@qq.com>
2025-11-29 15:29:06 +0800

    项目2

--- js/index.js
+++ js/index.js
@@ -1,59 +1,50 @@
-/**
- * 乐购商城首页js
- * 2025-11-26 by wy
- */
-// 当前页面加载完毕
-$(function(){
-	// 首页大图轮播
-    $('#banner').tyslide({
-				boxh:460,//盒子的高度
-				w:1000,//盒子的宽度
-				h:390,//图片的高度
-				isShow:true,//是否显示控制器
-				isShowBtn:true,//是否显示左右按钮
-				controltop:40,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
-				controlsW:20,//控制按钮宽度
-				controlsH:20,//控制按钮高度
-				radius:10,//控制按钮圆角度数
-				controlsColor:"#d7d7d7",//普通控制按钮的颜色
-				controlsCurrentColor:"#ff6600",//当前控制按钮的颜色
-				isShowNum:true //是否显示数字
-			});
+window.onload = function(){
+    $("#aside li").on("mouseover",function(){
+        $(".sub").css({"display":"block"}).appendTo(this);
+        $(this).css({
+            "background":"#fff",
+            border:"1px solid #f60",
+        });
+        let index = $(this).index();
+        $(this).find("span").css({
+            top:index*31+1+"px",
+            display:"block"
+        })
+    }).on("mouseout",function(){
+        $(this).find(".sub").css({"display":"none"});
+        $(this).css({
+            "background":"#f3f3f3",
+            border:"0"
+        });
+        $(this).find("span").css({
+            display:"none"
+        })
+    })
+    
+    //轮播
+    //获取按钮注册事件实现轮播图的切换 封装成一个函数
 
-	// 图书电子小轮播
-	$('.ebooks-banner').tyslide({
-				boxh:223,//盒子的高度
-				w:332,//盒子的宽度
-				h:223,//图片的高度
-				isShow:true,//是否显示控制器
-				isShowBtn:true,//是否显示左右按钮
-				controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
-				controlsW:20,//控制按钮宽度
-				controlsH:2,//控制按钮高度
-				controlsColor:"#d7d7d7",//普通控制按钮的颜色
-				controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
-			});
-	// 电子书table切换
-	var $lis = $('.ebooks ebooks-nav > li');
-	$lis.mouseover(function(){
-		//给自己添加激活类active 把兄弟的都干掉
-		$(this).addClass('active').siblings().removeClass('active');
-		//获取index
-		var index = $(this).index();
-		//选择内容
-		var $ebooksList = $('.ebooks-list');
-		//显示索引等于当前index
-		$ebooksList.eq(index).show().siblings('.ebooks-list').hide();
-	})
-	// 新书列表手风琴效果
-	$('.ebooks .rigt-box ul>li').mouseenter(function(){
-		//所以兄弟：隐藏详情 显示标题
-		$(this).siblings().find('.desc').hide();//隐藏详情
-		$(this).siblings().find('.ebooks-title').show();//显示标题
-		//当前：隐藏标题 显示详情
-		$(this).find('.ebooks-title').hide();//隐藏标题
-		$(this).find('desc').show();//显示标题
-	})
+    //大轮播
+    let ban1 = ["img/banner1.jpg","img/banner2.jpg","img/banner3.png","img/banner4.png","img/banner5.png","img/banner6.png"];
+    let imgIndex = 0;
+    let $img = $("#banner img");
+    let $li = $("#bottomBtn li");
+    let $leftBtn = $("#banner span:eq(0)");
+    let $rightBtn = $("#banner span:eq(1)");
+    let ac = "active" ;
+    slider($leftBtn,$rightBtn,$li,$img,ban1,imgIndex,ac);
+
+
+    //books 轮播
+    let bPng = ["img/335x220_ljx_0729.jpg","img/dsshbanner-zr-0914.jpg","img/0804zhu.jpg","img/jingguan.jpg"];
+    let $sImg = $("#books .banner img");
+    let $sli = $("#books .banner li");
+    let $sLeftBtn = $("#books .banner span:eq(0)");
+    let $sRightBtn = $("#books .banner span:eq(1)");
+    let liOn = "lion";
+    slider($sLeftBtn,$sRightBtn,$sli,$sImg,bPng,imgIndex,liOn);
+
+
     //dress
     let dressPng = ["img/139430026166665_y.jpg","img/162350015511487_y.jpg","img/72190020507327_1_o.jpg","img/68700016969136_y.jpg"];
     let $dressImg = $("#dress .banner img");
@@ -173,5 +164,7 @@ $(function(){
             clearInterval(time)
         }
         },30)
+        
+
     })
-})
+}
